a64f70cd74788f0c2af6c26186b089fb0aa001be
TheRealWalrus <baranyferenc2@gmail.com>
2019-12-25 19:22:06 +0100

    first commit

---
+++ Assets/Scripts/Cube Script/CubeMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    private Rigidbody myBody;
+
+    public float speed;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h != 0)
+        {
+            myBody.AddForce(new Vector3(speed * h, 0, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+
+    public float xBound;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 1));
+        Instantiate(enemy, new Vector3(Random.Range(-xBound, xBound), transform.position.y, transform.position.z), Quaternion.identity);
+        StartCoroutine(SpawnEnemy());
+
+    }
+}
